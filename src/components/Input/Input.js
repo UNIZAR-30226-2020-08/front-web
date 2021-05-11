34344f9e6825a728ffb73433d8a5eec06f94478e
pablogvrcia <pablo2000zgz@gmail.com>
2021-05-11 10:14:46 +0200

    Cambios esteticos chat

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import SendIcon from '@material-ui/icons/Send';
+import IconButton from '@material-ui/core/IconButton';
 
 import './Input.css';
 
@@ -7,12 +9,14 @@ const Input = ({ setMessage, sendMessage, message }) => (
     <input
       className="input"
       type="text"
-      placeholder="Type a message..."
+      placeholder="Escribe un mensaje..."
       value={message}
       onChange={({ target: { value } }) => setMessage(value)}
       onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
     />
-    <button className="sendButton" onClick={e => sendMessage(e)}>Send</button>
+    <IconButton className="sendButton" onClick={e => sendMessage(e)}>
+      <SendIcon />
+    </IconButton>
   </form>
 )
 
