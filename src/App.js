e829f56a1e7d07aa6a842352675a86dca71169a5
pablogvrcia <pablo2000zgz@gmail.com>
2021-05-21 13:24:48 +0200

    build version deploy

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ import Game from './components/pages/Game';
 import SignUp from './components/pages/SignUp';
 import Profile from './components/pages/Profile';
 import EditProfile from './components/pages/EditProfile';
-import CreateTournament from './components/pages/CreateTournament';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -27,7 +26,6 @@ function App() {
           <Route path='/inicio' exact component={Game} />
           <Route path='/profile' exact component={Profile} />
           <Route path='/editprofile' exact component={EditProfile} />
-          <Route path='/createtournament' exact component={CreateTournament} />
         </Switch>
         <Footer />
       </Router>
